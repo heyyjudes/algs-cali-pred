9b493b9632ca8e39e36e9e8b599d363475004d01
JHS <heyyjudes@outlook.com>
2025-01-24 12:11:32 -0800

    adding opt threshold

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestRegressor
 from sklearn.linear_model import ElasticNet, SGDRegressor, BayesianRidge, LinearRegression
-from sklearn.svm import SVR
+from sklearn.svm import SVR, SVC
 from xgboost.sklearn import XGBRegressor
 from sklearn.kernel_ridge import KernelRidge
 
@@ -27,17 +27,19 @@ clf_dict = {
     "XGB": xgb.XGBClassifier,
     "KNN": KNeighborsClassifier,
     "RF": RandomForestClassifier,
+    "SVM": SVC,
+    "NN": MLPClassifier,
 }
 
 reg_dict = {
     "LR": LinearRegression,
     "BR": BayesianRidge,
-    "xgb": XGBRegressor,
-    # "kernel": KernelRidge,
-    # "SVR": SVR,
-    # "sgd": SGDRegressor,
-    # "en":  ElasticNet,
-    # "RFR": RandomForestRegressor
+    "XGBR": XGBRegressor,
+    #"kernel": KernelRidge, # takes too long and bad R^2
+    #"SVR": SVR, # takes too long and bad R^2
+    "SGDR": SGDRegressor,
+    "EN":  ElasticNet,
+    #"RFR": RandomForestRegressor # takes too long and bad R^2
 }
 
 def get_best_classifier(X_train: np.array,
@@ -79,32 +81,70 @@ def get_best_regressor(X_train: np.array,
     print(f"best regressor score: {best_model} {best_score}")
     return best_model
 
+
 def model_choice(clf, xtrain=None, ytrain=None):
     param_grid_knn = {
         "knn__n_neighbors": [3, 5, 7]
+    }
+    param_grid_nn = {
+        "mlp__alpha": [0.05, 0.1],
+        "mlp__learning_rate": ["constant", "adaptive"],
+        'mlp__hidden_layer_sizes': [(8, 2)]
     }
     if clf == "XBG":
         model = make_pipeline(
             StandardScaler(), clf_dict[clf](objective="binary:logistic")
         )
-    elif clf == "KNN":
-        model = Pipeline(
+    elif clf == "SVM":
+        model = make_pipeline(
+            StandardScaler(), clf_dict[clf](probability=True)
+        )
+    elif clf == "NN":
+        temp_model = Pipeline(
             [
                 ("scalar", StandardScaler()),
-                ("knn", KNeighborsClassifier()),
+                (
+                    "mlp",
+                    MLPClassifier(
+                        solver="sgd",
+                        hidden_layer_sizes=(8, 2),
+                        random_state=1,
+                        max_iter=500,
+                    ),
+                ),
             ]
         )
-        print("running model search")
-        grid_search = GridSearchCV(model, param_grid_knn, n_jobs=-1, cv=5)
 
+        print("running model search")
+        grid_search = GridSearchCV(temp_model, param_grid_nn, n_jobs=-1, cv=5)
         grid_search.fit(xtrain, ytrain)
-        # final model
-        model = Pipeline(
-            [
-                ("scalar", StandardScaler()),
-                ("knn", KNeighborsClassifier(grid_search.best_params_["knn__n_neighbors"])),
-            ]
-        )
+        model = Pipeline([('scaling', StandardScaler())])
+        model.steps.append(("mlp", MLPClassifier(
+            solver="sgd",
+            hidden_layer_sizes=grid_search.best_params_["mlp__hidden_layer_sizes"],
+            random_state=1,
+            max_iter=500,
+            alpha=grid_search.best_params_["mlp__alpha"],
+            learning_rate=grid_search.best_params_["mlp__learning_rate"],
+        )))
+    elif clf == "KNN":
+            model = Pipeline(
+                [
+                    ("scalar", StandardScaler()),
+                    ("knn", KNeighborsClassifier()),
+                ]
+            )
+            print("running model search")
+            grid_search = GridSearchCV(model, param_grid_knn, n_jobs=-1, cv=5)
+
+            grid_search.fit(xtrain, ytrain)
+            # final model
+            model = Pipeline(
+                [
+                    ("scalar", StandardScaler()),
+                    ("knn", KNeighborsClassifier(grid_search.best_params_["knn__n_neighbors"])),
+                ]
+            )
     else:
         model = make_pipeline(StandardScaler(), clf_dict[clf]())
     return model
